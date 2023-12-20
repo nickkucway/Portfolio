b003b4433763f4489f88f2bbd80f4542fe76d85a
Nickkucway <nickkucway@gmail.com>
2023-12-20 14:07:08 -0600

    added modal, reached MVPgit add -A

---
+++ app.js
@@ -0,0 +1,20 @@
+//Grabbing Elements
+const openBtn = document.getElementById('open-modal');
+const modal = document.getElementById('modal')
+const closeBtn = document.getElementById('close')
+
+//Functions
+const openModal = () => {
+  modal.style.display = 'block';
+}
+
+const closeModal = () => {
+  modal.style.display = 'none'
+}
+
+//Event Listeners
+openBtn.addEventListener('click', openModal)
+
+closeBtn.addEventListener('click', closeModal)
+
+console.log('swag')
